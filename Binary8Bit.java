c824801181041a4e7656443b646f81b95e0444c2
StephanieTania <7314072@student.unpar.ac.id>
2015-10-21 16:22:39 +0700

    Create Binary8Bit.java

---
+++ Binary8Bit.java
@@ -0,0 +1,23 @@
+public class Binary8Bit{
+  private Binary4Bit[] data;
+  public Binary8Bit(Binary4Bit[] data){
+    this.data=data;
+  }
+  
+  public Binary4Bit[] getData(){
+    return this.data;
+  }
+  
+  public void setData(Binary4Bit[] data){
+    this.data=data;
+  }
+  
+  public boolean getDataAt(int idx){
+    if(idx<4){
+      return this.data[0].getDataAtIndex(idx);
+    }
+    else{
+      return this.data[1].getDataAtIndex(idx+4);
+    }
+  }
+}
