3abb931b46945bcf7f6c930afada20e707f068e2
Gabriella <gabriella.vg12@gmail.com>
2015-10-21 20:52:57 +0700

    Update ControlUnitSequencer.java

--- ControlUnitSequencer.java
+++ ControlUnitSequencer.java
@@ -1,15 +1,55 @@
-import java.util.LinkedList;
+
 public class ControlUnitSequencer{
   
-  private LinkedList<Integer> bits;
+  // Atribut yang menyimpan nilai [Cp,Ep,Lm',CE'],[Li',Ei',La',Ea],[Su,Eu,Lb',Lo']  
+    private int[] bits;
     
     public ControlUnitSequencer() {
-        bits = new LinkedList<Integer>();
+        // Nilai awal bits = 0011 1110 0011
+        int[] values = {0,0,1,1,1,1,1,0,0,0,1,1};
+        bits = values;
     }
     
+    /**
+     * Method untuk mendapatkan nilai dari atribut bits
+     * @param index posisi nilai yang dicari
+     * @return nilai dari bits ke-[index]
+     */
     public int getBitsAt(int index) {
-        return bits.get(index);
+        return bits[index];
     }
     
+    /**
+     * Method untuk mengubah nilai dari atribut bits
+     * @param index posisi nilai yang ingin diubah
+     */
+    private void changeValueAt(int index) {
+        switch(getBitsAt(index)) {
+            case 0:
+                setValueAt(index,1);
+                break;
+            case 1:
+                setValueAt(index,0);
+                break;
+        }
+    }
+        
+    /**
+     * Method untuk melakukan set nilai dari atribut bits
+     * @param index posisi nilai yang ingin di set
+     * @param input nilai untuk bits ke-[index]
+     */    
+    private void setValueAt(int index, int input) {
+        bits[index] = input;
+    }
+    
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i=0; i<bits.length; i++){
+            result+= String.valueOf(getBitsAt(i));
+        }
+        return result;
+    }
   
 }
