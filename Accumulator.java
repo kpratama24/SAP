ce989ebce13dfa1d25c7c041bb0dac0343bef81c
Kevin Pratama <admin@blankevin.com>
2015-10-21 23:12:36 +0700

    Update Accumulator.java
    please define 8bit class as "Bit8"

--- Accumulator.java
+++ Accumulator.java
@@ -1,2 +1,11 @@
 public class Accumulator{
+  private Bit8 data;
+
+    public void setData(Bit8 data) {
+        this.data = data;
+    }
+
+    public Bit8 getData() {
+        return this.data;
+    }
 }
