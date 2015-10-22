d5cb5b55e126679fd241d062d15f9c6080509cd1
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-22 16:32:04 +0700

    Update BRegister.java

--- BRegister.java
+++ BRegister.java
@@ -1,2 +1,13 @@
 public class BRegister{
+ private int bit; //menyimpan bit dari RAM
+ 
+ public BRegister(int bit){
+  this.bit=bit;
+ }
+ public int getBit(){
+  return this.bit;
+ }
+ public void setBit(int bit){
+  this.bit=bit;
+ }
 }
