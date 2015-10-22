6e5b5fa0b2c5bb465570983e4302b3ab21f16853
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 16:34:30 +0700

    Update Bit.java

--- Bit.java
+++ Bit.java
@@ -8,23 +8,58 @@ public class Bit{
    */
   protected String value;
   
+  /**
+   * Constructor kelas Bit
+   */
   public Bit(String value){
     this.value=value;
   }
   
+  /**
+   * Method getter atribut value
+   * @return string berupa bilangan biner
+   */
   public String getValue(){
     return this.value;
   }
   
+  /**
+   * Method setter untuk atribut value
+   * @param value bilangan biner baru
+   */
   public void setValue(String value){
     this.value=value;
   }
   
+  /**
+   * Method untuk mengubah bilangan biner ke bilangan desimal
+   * @return nilai desimal dari bilangan biner yang ada
+   */
   public int toDecimal(){
-    return String.valueOf(this.value);
+    int res=0;
+    for(int i=this.value.length()-1;i>=0;i--){
+      if(this.value.charAt(i)=='1'){
+        res+=Math.pow(2,this.value.length()-1-i);
+      }
+    }
+    return res;
   }
   
-  public String toBinary(int val){
-    return val+"";
+  /**
+   * Method untuk mengubah bilangan desimal ke bilangan biner
+   * @param val bilangan desimal yang akan diubah ke biner
+   * @param bitLength panjang bilangan biner
+   * @return string berupa bilangan biner dari bilangan desimal yang diberikan oleh user
+   */
+  public String toBinary(int val,int bitLength){
+    String str="";
+    while(val>0&&str.length()<bitLength){
+      str=(val%2)+str;
+      val/=2;
+    }        
+    while(str.length()<bitLength){
+      str='0'+str;
+    }
+    return str;
   }
 }
