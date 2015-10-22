7243725ea2f14bd5e70c3729ea53de8327fa3bf3
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 17:06:17 +0700

    Update ProgramCounter.java

--- ProgramCounter.java
+++ ProgramCounter.java
@@ -1,19 +1,40 @@
+/**
+ * Kelas yang merepresentasikan program counter
+ */
 public class ProgramCounter{
+  
+  /**
+   * Nilai counter saat ini
+   */
   private int count;
   
+  /**
+   * Constructor kelas ProgramCounter
+   */
   public ProgramCounter(){
     this.count=0;
   }
   
+  /**
+   * Method untuk menambah nilai counter saat ini
+   */
   public void countUp(){
     this.count++;
   }
   
+  /**
+   * Method untuk mengatur counter saat ini
+   * @param nilai counter yang baru
+   */
   public void setCount(int count){
     this.count=count;
   }
   
-  public int getCount(){
-    return this.count;
+  /**
+   * Method untuk melihat nilai counter saat ini
+   * @return nilai counter saat ini dalam bilangan biner 4 bit
+   */
+  public String getCount(){
+    return (new Bit4(this.count)).getValue();
   }
 }
