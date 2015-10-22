d2024801633e364a999af68bf6c9cce22c19ee38
Gabriella <gabriella.vg12@gmail.com>
2015-10-22 23:04:05 +0700

    Update InstructionRegister.java

--- InstructionRegister.java
+++ InstructionRegister.java
@@ -1,2 +1,28 @@
-public class InstructionRegister{
+public class InstructionRegister {
+    
+    private Bit8 opCode;
+    private Bit4 operand;
+    
+    public InstructionRegister(Bit8 input) {
+        this.opCode = input;
+        String binary = opCode.toBinary(input.toDecimal(), 8);
+        this.operand = new Bit4(binary.substring(4));
+    }
+
+    public Bit8 getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(Bit8 opCode) {
+        this.opCode = opCode;
+    }
+
+    public Bit4 getOperand() {
+        return operand;
+    }
+
+    public void setOperand(Bit4 operand) {
+        this.operand = operand;
+    }
+    
 }
