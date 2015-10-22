6af163804993bf0c01acee240c8e3ee89989cba9
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 15:40:06 +0700

    Update Bit4.java

--- Bit4.java
+++ Bit4.java
@@ -1,2 +1,22 @@
+/**
+ * Kelas yang merepresentasikan 4 bit angka biner
+ */
 public class Bit4{
+  
+  /**
+   * array angka biner 1 bit berukuran 4
+   */
+  private Bit1[] value;
+  
+  public Bit4(Bit1 value){
+    this.value=value;
+  }
+  
+  public Bit1[] getValue(){
+    return this.value;
+  }
+  
+  public void setValue(Bit1[] value){
+    this.value=value;
+  }
 }
