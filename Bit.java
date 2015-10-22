4d5884d2dc37b84bdfe08d35952ce37548a8a618
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 16:06:22 +0700

    Update and rename Bit1.java to Bit.java

---
+++ Bit.java
@@ -0,0 +1,35 @@
+/**
+ * Kelas yang merepresentasikan 1 bit angka biner
+ */
+public class Bit{
+  
+  /**
+   * Nilai yang direpresentasikan dengan biner 1 bit
+   */
+  private String value;
+  
+  public Bit(String value){
+    this.value=value;
+  }
+  
+  public String getValue(){
+    return this.value;
+  }
+  
+  public void setValue(String value){
+    this.value=value;
+  }
+  
+  public int toDecimal(){
+    return String.valueOf(this.value);
+  }
+  
+  public String toBinary(int val){
+    return val+"";
+  }
+  
+  @Override
+  public String toString(){
+    return this.getValue();
+  }
+}
