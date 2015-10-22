83882187e02ab4782e56a8221303335f8a768b8d
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 12:54:21 +0700

    Update Bit1.java

--- Bit1.java
+++ Bit1.java
@@ -1,2 +1,22 @@
+/**
+ * Kelas yang merepresentasikan 1 bit angka biner
+ */
 public class Bit1{
+  
+  /**
+   * Nilai yang direpresentasikan dengan biner 1 bit
+   */
+  private int value;
+  
+  public Bit1(int value){
+    this.value=value;
+  }
+  
+  public int getValue(){
+    return this.value;
+  }
+  
+  public void setValue(int value){
+    this.value=value;
+  }
 }
