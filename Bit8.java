0f7995102d2def2960ba3b4bda5005bfeceb9d35
StephanieTania <7314072@student.unpar.ac.id>
2015-10-22 15:43:47 +0700

    Update Bit8.java

--- Bit8.java
+++ Bit8.java
@@ -1,2 +1,22 @@
+/**
+ * Kelas yang merepresentasikan 8 bit angka biner
+ */
 public class Bit8{
+  
+  /**
+   * array angka biner 4 bit berukuran 2
+   */
+  private Bit4[] value;
+  
+  public Bit8(Bit4[] value){
+    this.value=value;
+  }
+  
+  public Bit4[] getValue(){
+    return this.value;
+  }
+  
+  public void setValue(Bit4[] value){
+    this.value=value;
+  }
 }
