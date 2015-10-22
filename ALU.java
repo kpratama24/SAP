ce97bb232f66f9d9fabec030c4fbc34a48d6da0a
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-22 16:41:33 +0700

    Update ALU.java

--- ALU.java
+++ ALU.java
@@ -1,2 +1,14 @@
 public class ALU{
+ //index Su yang didapat dari ControlUnitSequencer
+ private final int IDXSU=8;
+
+ public void calculate(Accumulator acc, BRegister br){
+  ControlUnitSequencer cu=new ControlUnitSequencer();
+  int su=cu.getBitsAt(IDXSU);
+  if(su==1){
+
+  } else{
+
+  }
+ }
 }
