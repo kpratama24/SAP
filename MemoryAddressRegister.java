17fb1eed042f6ac637fbd1e32a3c286033049927
Kevin Pratama <admin@blankevin.com>
2015-10-23 07:57:04 +0700

    Update MemoryAddressRegister.java
    add the javadoc

--- MemoryAddressRegister.java
+++ MemoryAddressRegister.java
@@ -1,2 +1,20 @@
 public class MemoryAddressRegister{
+    
+    private Bit4 data;
+
+  /**
+   * Setter for the data
+   * @param data the value that given by the user
+   */ 
+    public void setData(Bit4 data) {
+        this.data = data;
+    }
+
+  /**
+   * Getter for the data
+   * @return the value for the data
+   */ 
+    public Bit4 getData() {
+        return this.data;
+    }
 }
