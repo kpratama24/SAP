ffc869824d5110f0f8f211d15f11852f1b2e3f31
Kevin Pratama <admin@blankevin.com>
2015-10-23 08:35:07 +0700

    Update Main.java
    add the tester

--- Main.java
+++ Main.java
@@ -4,4 +4,23 @@
  */
 public class Main{
 
+    String[] memory = new String[16];
+        memory[0] = "00001001";
+        memory[1] = "00011010";
+        memory[2] = "00011011";
+        memory[3] = "00011100";
+        memory[4] = "00101101";
+        memory[5] = "11100000";
+        memory[6] = "11110000";
+        memory[7] = "00000000";
+        memory[8] = "00000000";
+        memory[9] = "00010000";
+        memory[10] = "00010100";
+        memory[11] = "00011000";
+        memory[12] = "00011100";
+        memory[13] = "00100000";
+        memory[14] = "00000000";
+        memory[15] = "00000000";
+        SAPSimulator simulator = new SAPSimulator(memory);
+        simulator.start();
 }
