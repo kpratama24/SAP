892b8596b8bfed289d6b1b9103f0a9e295a80970
Kevin Pratama <admin@blankevin.com>
2015-10-23 07:58:56 +0700

    Update OutputRegister.java
    add the javadoc and updated the layout of the code

--- OutputRegister.java
+++ OutputRegister.java
@@ -1,2 +1,21 @@
 public class OutputRegister{
+   private Bit8 data;
+
+    /**
+    * Getter for the data
+    * @return the data value
+    */ 
+    public Bit8 getData() {
+        return this.data;
+    }
+    
+    /**
+     * Setter for the data
+     * @param data the value of the data
+     */
+    public void setData(Bit8 data) {
+        this.data = data;
+    }
+
+   
 }
