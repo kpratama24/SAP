856b11725a3307b63ab13e0542f530aa2667350f
Kevin Pratama <admin@blankevin.com>
2015-10-23 08:10:46 +0700

    Update Bit4.java
    update the javadoc

--- Bit4.java
+++ Bit4.java
@@ -1,11 +1,12 @@
 /**
- * Kelas yang merepresentasikan 4 bit angka biner
+ * Class that represent 4 bit of binary number
  */
 public class Bit4 extends Bit{
   
   /**
-   * Constructor kelas Bit4
-   * @param value string biner 4 bit
+   * Bit4 Class Constructor
+   * this Constructor will override the value of "value" at the bit class
+   * @param value 4Bit binary in the form of String data type
    */
   public Bit4(String value){
     super(value);
@@ -13,7 +14,7 @@ public class Bit4 extends Bit{
   
   /**
    * Constructor kelas Bit4
-   * @param value bilangan desimal sebagai data 4 bit
+   * @param value the decimal number as the Bit4 data type
    */
   public Bit4(int value){
     super(value,4);
