a716605114200a443bcef418b4378040ac075b01
Kevin Pratama <admin@blankevin.com>
2015-10-23 08:07:16 +0700

    Update BinaryDisplay.java
    modify the javadoc according to java guidelines

--- BinaryDisplay.java
+++ BinaryDisplay.java
@@ -1,2 +1,24 @@
 public class BinaryDisplay{
+  private Bit8 data;
+
+  /**
+  * setter for the Bit8 data
+  */
+    public void setData(Bit8 data) {
+        this.data = data;
+    }
+  
+  /**
+   * Getter for the Bit8 data
+   */
+    public Bit8 getData() {
+        return this.data;
+    }
+
+  /**
+   * Method for printing the data int the form of decimal number
+   */
+    public void print() {
+        System.out.println("Binary display = " + data.toDecimal());
+    }
 }
