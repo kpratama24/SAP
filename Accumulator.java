88b985ba21f9c9f1ded1e970184d33e2342d8dcd
Gabriella <gabriella.vg12@gmail.com>
2015-10-23 07:35:15 +0700

    Update Accumulator.java

--- Accumulator.java
+++ Accumulator.java
@@ -1,11 +1,15 @@
 public class Accumulator{
   private Bit8 data;
-
-    public void setData(Bit8 data) {
+  
+  public Accumulator(Bit8 data) {
         this.data = data;
     }
 
-    public Bit8 getData() {
-        return this.data;
-    }
+  public void setData(Bit8 data) {
+      this.data = data;
+  }
+
+  public Bit8 getData() {
+      return this.data;
+  }
 }
