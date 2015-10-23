c1b3f01faf28cdceda87b5a1e47da3f9a217a5c3
Gabriella <gabriella.vg12@gmail.com>
2015-10-23 08:01:33 +0700

    Update ALU.java

--- ALU.java
+++ ALU.java
@@ -1,14 +1,51 @@
-public class ALU{
+public class ALU {
+
  //index Su yang didapat dari ControlUnitSequencer
  private final int IDXSU=8;
 
- public void calculate(Accumulator acc, BRegister br){
-  ControlUnitSequencer cu=new ControlUnitSequencer();
-  int su=cu.getBitsAt(IDXSU);
-  if(su==1){
+// Atribut yang menyimpan bit yang didapat dari Accumulator dan BRegister
+    private Bit8 ACCData, BRegData;
 
-  } else{
-
-  }
- }
+    public ALU(Accumulator acc, BRegister br) {
+        this.ACCData = acc.getData();
+        this.BRegData = br.getData();     
+    }
+    
+    /**
+     * method untuk melakukan operasi penjumlahan atau pengurangan
+     * antara ACCData dan BRegData
+     * @return hasil dari operasi perhitungan yang diinginkan
+     */
+    public Bit8 calculate() {
+        ControlUnitSequencer cu = new ControlUnitSequencer();
+        switch(cu.getBitsAt(IDXSU)) {
+            case 0:
+                return this.add();
+            case 1:
+                return this.substract();
+            default:
+                return null;
+        }
+    }
+    
+    /**
+     * Method untuk melakukan operasi penjumlahan
+     * antara ACCData dan BRegData
+     * @return hasil penjumlahan
+     */
+    private Bit8 add() {
+        int result = ACCData.toDecimal() + BRegData.toDecimal();
+        return new Bit8(result);
+    }
+    
+    /**
+     * Method untuk melakukan operasi pengurangan
+     * antara ACCData dan BRegData
+     * @return hasil pengurangan
+     */
+    private Bit8 substract() {
+        int result = ACCData.toDecimal() - BRegData.toDecimal();
+        return new Bit8(result);
+    }
+    
 }
