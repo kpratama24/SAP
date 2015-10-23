1f388493bc5cdc417d3b04a9d0fb953d27cf8bd0
StephanieTania <7314072@student.unpar.ac.id>
2015-10-23 11:36:20 +0700

    Update ProgramCounter.java

--- ProgramCounter.java
+++ ProgramCounter.java
@@ -6,27 +6,27 @@ public class ProgramCounter{
   /**
    * Display the current counter value
    */
-  private int count;
+  private Bit4 count;
   
   /**
    * ProgramCounter Class Constructor
    */
   public ProgramCounter(){
-    this.count=0;
+    this.count=new Bit4(0);
   }
   
   /**
    * Method to add the counter
    */
   public void countUp(){
-    this.count++;
+    this.count=this.count.setValue(this.count.toBinary(this.count.toDecimal()+1,4));
   }
   
   /**
    * Method for setting a new counter value based on user input
    * @param new counter value
    */
-  public void setCount(int count){
+  public void setCount(Bit4 count){
     this.count=count;
   }
   
@@ -34,7 +34,7 @@ public class ProgramCounter{
    * Method for getting the current counter
    * @return current counter value in a form of 4Bit number
    */
-  public String getCount(){
-    return (new Bit4(this.count)).getValue();
+  public Bit4 getCount(){
+    return this.count;
   }
 }
