716262300dbc9cfd33b671b637102a15a7f73e86
Gabriella <gabriella.vg12@gmail.com>
2015-10-23 08:05:55 +0700

    Update BRegister.java

--- BRegister.java
+++ BRegister.java
@@ -1,28 +1,28 @@
 public class BRegister{
- private String bit; // Save the bit from the RAM
+ private Bit8 data; // Save the bit from the RAM
  
  /**
   * The class cnstructor of BufferRegister
   * @param bit the bit input that will be enetered to BufferRegister
   */
- public BRegister(String bit){
-   this.bit=bit;
+ public BRegister(Bit8 bit){
+   this.data = bit;
  }
 
  /**
-  * Getter for the bit attributes
-  * @return the bit value in the form of String data type
+  * Getter for the data attributes
+  * @return the bit value in the form of Bit8 data type
   */
- public String getBit(){
-  return this.bit;
+ public Bit8 getData() {
+  return this.data;
  }
 
  /**
   * Setter for the bit attributes
   * @param bit input that will be applied to the bit attributes
   */
- public void setBit(String bit){
-  this.bit=bit;
+ public void setBit(Bit8 bit){
+  this.data = bit;
  }
 
  /**
@@ -30,7 +30,6 @@ public class BRegister{
   * @return the bit in a form of decimal number
   */
  public int getBitToDecimal(){
-  Bit bits=new Bit(bit);
-  return bits.toDecimal();
+  return data.toDecimal();
  }
 }
