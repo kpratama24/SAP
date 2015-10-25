ff42d0a4ea934a3dc15bcea90129e99023727ef6
StephanieTania <7314072@student.unpar.ac.id>
2015-10-25 23:17:31 +0700

    Update InstructionRegister.java

--- InstructionRegister.java
+++ InstructionRegister.java
@@ -5,24 +5,26 @@
 public class InstructionRegister {
     
     
-    private Bit8 opCode;
+    private Bit4 opCode;
     private Bit4 operand;
+    private Bit8 ramData;
     
     /**
      * Class Constructor
      * @param input the Bit8 number
      */
     public InstructionRegister(Bit8 input) {
-        this.opCode = input;
-        String binary = opCode.toBinary(input.toDecimal(), 8);
-        this.operand = new Bit4(binary.substring(4));
+        this.ramData = input;
+        //String binary = opCode.toBinary(input.toDecimal(), 8);
+        this.opCode = new Bit4(input.getValue().substring(0,4));
+        this.operand = new Bit4(input.getValue().substring(4));
     }
 
     /**
     * Getter for the opCode
     * @return the opCode
     */
-    public Bit8 getOpCode() {
+    public Bit4 getOpCode() {
         return opCode;
     }
     
@@ -30,7 +32,7 @@ public class InstructionRegister {
      * Setter for the opcode
      * @param opCode the value that given by the user
      */
-    public void setOpCode(Bit8 opCode) {
+    public void setOpCode(Bit4 opCode) {
         this.opCode = opCode;
     }
 
@@ -50,4 +52,11 @@ public class InstructionRegister {
         this.operand = operand;
     }
     
+    public Bit8 getRAMData(){
+        return this.ramData;
+    }
+    
+    public void setRAMData(Bit8 newData){
+        this.ramData=newData;
+    }
 }
