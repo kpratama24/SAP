ab8226d67e9aacfe5fe19ea3c92e35b738b2947f
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 10:58:37 +0700

    Update SAPSimulator.java

--- SAPSimulator.java
+++ SAPSimulator.java
@@ -24,25 +24,50 @@ public class SAPSimulator{
     alu=new ALU();
   }
   
-  public void start(){
+  public void start(PrintStream out){
     // simulation
     
     // Initial state - T0
     cu.setState(0);
+    out.println(this);
     
     // Address state - T1
     cu.setState(1);
+    mar.setData(pc.getCount());
+    out.println(this);
     
     // Increment state - T2
     cu.setState(2);
+    pc.countUp();
+    out.println(this);
     
     // Memory state - T3
     cu.setState(3);
+    ir.setRAMData(mar.getRAM());
+    out.println(this);
     
-    // State T4 depends on command (LDA, ADD, SUB, OUT, HLT)
+    // Memory state - T4
+    cu.setState(4);
     
-    // State T5 depends on command (LDA, ADD, SUB, OUT, HLT)
     
-    // State T6 depends on command (LDA, ADD, SUB, OUT, HLT)
+    // Memory state - T5
+    cu.setState(5);
+    
+    // Memory state - T6
+    cu.setState(6);
+  }
+  
+  public String toString(){
+    String result="";
+    result+="PC  : "+pc.toString()+"\n";
+    result+="MAR : "+mar.toString()+"\n";
+    result+="IR  : "+ir.toString()+"\n";
+    result+="CU  : "+cu.toString()+"\n";
+    result+="ACC : "+acc.toString()+"\n";
+    result+="ALU : "+alu.toString()+"\n";
+    result+="RB  : "+bReg.toString()+"\n";
+    result+="OR  : "+or.toString()+"\n";
+    result+="BD  : "+binDisplay.toString()+"\n";
+    return result;
   }
 }
