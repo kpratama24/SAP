cdeaaab23b44f4c23fac4825e44bd23d39a2d693
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 21:20:46 +0700

    Update BRegister.java

--- BRegister.java
+++ BRegister.java
@@ -24,4 +24,15 @@ public class BRegister{
  public int getBitToDecimal(){
   return data.toDecimal();
  }
+ 
+ /**
+  * Method ini mengembalikan nilai dari data
+  * @return nilai data dalam biner jika tidak null, XXXXXXXX jika null
+  */
+ public String toString(){
+      if(data!=null){
+          return this.data.getValue();
+      }
+      return "XXXXXXXX";
+  }
 }
