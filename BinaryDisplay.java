541b67fcbf8302b6cf813a7019c420f0345e049c
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 21:23:44 +0700

    Update BinaryDisplay.java

--- BinaryDisplay.java
+++ BinaryDisplay.java
@@ -4,21 +4,32 @@ public class BinaryDisplay{
   /**
   * setter for the Bit8 data
   */
-    public void setData(Bit8 data) {
-        this.data = data;
-    }
+  public void setData(Bit8 data) {
+    this.data = data;
+  }
   
   /**
    * Getter for the Bit8 data
    */
-    public Bit8 getData() {
-        return this.data;
-    }
+  public Bit8 getData() {
+    return this.data;
+  }
 
   /**
    * Method for printing the data int the form of decimal number
    */
-    public void print() {
-        System.out.println("Binary display = " + data.toDecimal());
-    }
+  public void print() {
+    System.out.println("Binary display = " + data.toDecimal());
+  }
+  
+  /**
+     * Method ini mengembalikan nilai dari data
+     * @return nilai data dalam biner jika tidak null, XXXXXXXX jika null
+     */
+	 public String toString(){
+      if(data!=null){
+          return this.data.getValue();
+      }
+      return "XXXXXXXX";
+  }
 }
