101a68aeb9cfe724295fc83574c14fd7052ae5f5
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 21:33:26 +0700

    Update OutputRegister.java

--- OutputRegister.java
+++ OutputRegister.java
@@ -1,21 +1,30 @@
 public class OutputRegister{
    private Bit8 data;
 
-    /**
+   /**
     * Getter for the data
     * @return the data value
     */ 
-    public Bit8 getData() {
-        return this.data;
-    }
+   public Bit8 getData() {
+      return this.data;
+   }
     
-    /**
-     * Setter for the data
-     * @param data the value of the data
-     */
-    public void setData(Bit8 data) {
-        this.data = data;
-    }
+   /**
+    * Setter for the data
+    * @param data the value of the data
+    */
+   public void setData(Bit8 data) {
+      this.data = data;
+   }
 
-   
+   /**
+    * Method ini mengembalikan nilai dari data
+    * @return nilai data dalam biner jika tidak null, XXXXXXXX jika null
+    */
+	public String toString(){
+      if(data!=null){
+          return this.data.getValue();
+      }
+      return "XXXXXXXX";
+   }
 }
