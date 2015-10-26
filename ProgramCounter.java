8d6706c792924f459c26815047deeb984f0d7677
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 21:41:05 +0700

    Update ProgramCounter.java

--- ProgramCounter.java
+++ ProgramCounter.java
@@ -7,20 +7,23 @@ public class ProgramCounter{
    * Display the current counter value
    */
   private Bit4 count;
+  /**
+   * current counter value for MAR
+   */
+  private Bit4 ctMar;
   
   /**
    * ProgramCounter Class Constructor
    */
   public ProgramCounter(){
     this.count=new Bit4(0);
+    this.ctMar=new Bit4(0);
   }
   
   /**
    * Method to add the counter
    */
   public void countUp(){
-    //this.count=
-    //add error, "void cannot be converted to Bit4"
     this.count.setValue(this.count.toBinary(this.count.toDecimal()+1,4));
   }
   
@@ -38,5 +41,32 @@ public class ProgramCounter{
    */
   public Bit4 getCount(){
     return this.count;
+  }
+  
+  /**
+   * Method for getting the current counter for MAR
+   * @return current counter for MAR value in a form of 4Bit number
+   */
+  public Bit4 getCtMar() {
+    return ctMar;
+  }
+
+  /**
+   * Method for setting a new counter for MAR value based on user input
+   * @param new counter for MAR value
+   */
+  public void setCtMar(Bit4 ctMar) {
+    this.ctMar = ctMar;
+  }
+  
+  /**
+   * Method ini mengembalikan nilai dari data
+   * @return nilai data dalam biner jika tidak null, XXXXXXXX jika null
+   */
+	public String toString(){
+    if(count!=null){
+      return this.count.getValue();
+    }
+    return "XXXXXXXX";
   }
 }
