6fa9b478a412128d838f028d996c09f705f66bb0
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-10-26 21:31:34 +0700

    Update MemoryAddressRegister.java

--- MemoryAddressRegister.java
+++ MemoryAddressRegister.java
@@ -52,4 +52,15 @@ public class MemoryAddressRegister{
     public Bit4 getData() {
         return this.data;
     }
+    
+    /**
+     * Method ini mengembalikan nilai dari data
+     * @return nilai data dalam biner jika tidak null, XXXXXXXX jika null
+     */
+	public String toString(){
+      if(data!=null){
+          return this.data.getValue();
+      }
+      return "XXXXXXXX";
+    }
 }
