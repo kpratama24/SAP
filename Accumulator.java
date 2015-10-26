19b7a6416ae7aaa27dc3731082eb6b1b47aa6164
Ignasius Christopher A <i.christo@yahoo.co.id>
2015-10-26 21:14:15 +0700

    Update Accumulator.java

--- Accumulator.java
+++ Accumulator.java
@@ -1,10 +1,17 @@
 public class Accumulator{
+  //Tempat penyimpanan Bit8 dalam accumulator
   private Bit8 data;
-  
+  /**
+     * method untuk melakukan pengesetan data Bit8 pada accumulator
+     * @return void
+     */
   public void setData(Bit8 data) {
       this.data = data;
   }
-
+  /** 
+     * methode untuk mengambil data dari accumulator
+     * @return Bit8
+     */
   public Bit8 getData() {
       return this.data;
   }
